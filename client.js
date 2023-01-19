261855ec478e65155a13b2e3ab96c3ff23c5c1ce
anggidast <anggi@feedloop.io>
2023-01-19 12:21:49 +0700

    fix: service worker

--- client.js
+++ client.js
@@ -18,7 +18,7 @@ function getData(form) {
 async function send(email) {
   // Register Service Worker
   console.log('Registering service worker...');
-  const register = await navigator.serviceWorker.register('/web-push-identity-generator/worker.js', {
+  const register = await navigator.serviceWorker.register('/web-push-identity-generator/sw.js', {
     scope: '/web-push-identity-generator/',
   });
   console.log('Service Worker Registered...');
